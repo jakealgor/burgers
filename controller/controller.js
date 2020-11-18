6199b7e4eb7c8dd070cab37c474812f68a31b8f7
jakealgor <jake.algor@gmail.com>
2020-11-17 19:05:44 -0500

    first commit

---
+++ controller/controller.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const burger = require("../model/burger.js");
+
+let router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.all(function (burgerData) {
+        res.render("index", { burger_data: burgerData });
+    });
+});
+
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id", function (req, res) {
+    burger.update(req.params.id, function (result) {
+
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".ate").on("submit", function (event) {
+        event.preventDefault();
+        var burger_id = $(this).children(".burger_id").val();
+        $.ajax({
+            method: "PUT",
+            url: "/burgers/" + burger_id
+        }).then(function () {
+            location.reload();
+        });
+    });
+});
