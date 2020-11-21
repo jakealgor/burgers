5e5cd10caf89335930e68dc59e67dfd3f6b630b5
jakealgor <jake.algor@gmail.com>
2020-11-21 15:32:54 -0500

    heroku push fix connections

--- controller/controller.js
+++ controller/controller.js
@@ -18,7 +18,6 @@ router.post("/burgers/create", function (req, res) {
 
 router.put("/burgers/:id", function (req, res) {
     burger.update(req.params.id, function (result) {
-
         console.log(result);
         res.sendStatus(200);
     });
