051feff9242cd3a22b5f2a50ab08a0fac63c4b07
jakealgor <jake.algor@gmail.com>
2020-11-23 12:47:15 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 8000;
 var app = express();
 
-app.set("port", PORT)
 
 app.use(express.static("public"));
 
